8696802a12b2f14669ef1f6b82f90a8bb05dd12a
Ester Vojkollari <e.vojko@gmail.com>
2018-10-23 16:00:43 -0400

    implemented checkWinner vertically and horizontally

--- js/main.js
+++ js/main.js
@@ -33,7 +33,6 @@ function switchTurns(color){
 
 //makes a move. Takes a parameter that means which columns he's gonna put the disc
 function makeMove(column){
-  debugger;
  if(turn === 'blue'){
    switch(column){
      case 0 : placeDisk(0, 'blue');
@@ -86,17 +85,112 @@ function makeMove(column){
  }
 }
 
+//puts the value disk in the array
 function placeDisk(column, turn){
-  let j = board.length-1;
-  if( board[j][column] === null){
-    board[j][column] = turn;
+  let row = board.length-1;
+  if( board[row][column] === null){
+    board[row][column] = turn;
+    checkWinner(row,column);
   }
   else{
     for(let i=0; i<board.length; i+=1){
       if(board[i][column] !== null){
         board[i-1][column] = turn;
+        checkWinner(i-1, column);
         return;
       }
     }
+  }
+}
+
+//checks if 4 of the same in a row
+function checkWinner(row, column){
+  let checkHorizontally = checkHorizontally(row, column);
+  // let checkVertically = checkVertically(row, column);
+  if( checkHorizontally[0] === true){
+    return checkHorizontally;
+  }
+  // else if( checkVertically[0] === true){
+  //   return checkVertically;
+  // }
+
+}
+
+//checks if coordinates surrounding (row, column) are inside the board
+// function isInBoard(row, column){
+//   if( row-3 >= 0 && row+3 <=5 && column-3 >= 0 && column+3 <= 6){
+//     return true;
+//   }
+//   else{
+//     return false;
+//   }
+// }
+
+//checks horizontally left and right
+//if it doesnt work implement cases when its null
+function checkHorizontally(row, column){
+  //check left
+  if( column-3 >= 0 &&
+    board[row][column] === board[row][column-1] &&
+    board[row][column-1] === board[row][column-2] &&
+    board[row][column-2] === board[row][column-3]) {
+      return [true, "left"];
+    }
+  else if( column+3 <= 6 &&
+    board[row][column] === board[row][column+1] &&
+    board[row][column+1] === board[row][column+2] &&
+    board[row][column+2] === board[row][column+3])
+  {
+    return [true, "right"];
+  }
+  else{
+    return [false];
+  }
+}
+
+
+//checks vertically
+function checkVertically(row, column){
+  //check up
+  if( row-3 >= 0 &&
+    board[row][column] === board[row-1][column] &&
+    board[row-1][column] === board[row-2][column] &&
+    board[row-2][column] === board[row-3][column]) {
+      return [true, "up"];
+    }
+    //check down
+  else if( row+3 <= 5 &&
+    board[row][column] === board[row+1][column] &&
+    board[row+1][column] === board[row+2][column] &&
+    board[row+2][column] === board[row+3][column])
+  {
+    return [true, "down"];
+  }
+  else{
+    return [false];
+  }
+}
+
+function checkFirstDiagonal(row, column){
+  debugger;
+  if(board[row][column] === null){
+    return [false];
+  }
+  //check up
+  else if( row-3 >= 0 && column+3 <= 6 &&
+    board[row][column] === board[row-1][column+1] &&
+    board[row-1][column+1] === board[row-2][column+2] &&
+    board[row-2][column+2] === board[row-3][column+3]) {
+      return [true, "up"];
+    }
+  else if( row+3 <= 5 && column-3 >= 0 &&
+    board[row][column] === board[row+1][column-1] &&
+    board[row+1][column-1] === board[row+2][column-2] &&
+    board[row+2][column-2] === board[row+3][column-3])
+  {
+    return [true, "down"];
+  }
+  else{
+    return [false];
   }
 }
